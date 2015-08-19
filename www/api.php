4d074b95e8d3276b66dca7029cc0d3ed089674eb
stepanoff <stenlex@gmail.com>
2015-08-20 00:47:59 +0500

    Update api.php

--- www/api.php
+++ www/api.php
@@ -6,7 +6,7 @@ require_once '../vendor/autoload.php';
 $client = new Github\Client();
 $client->authenticate('user', 'password', 'http_password');
 
-$commits = $client->api('repo')->commits()->all('mediasite', 'gpor', array('sha' => 'master'));
+$commits = $client->api('repo')->commits()->all('organization', 'repo', array('sha' => 'master'));
 var_dump($commits);
 //$repositories = $client->api('user')->repositories('ornicar');
 ?>
